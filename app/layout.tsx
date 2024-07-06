2a4c3aacb7a3a317c4123ffed6a2d11ac4c7b953
Matt Whitney <matt@utopia.build>
2024-07-06 11:23:17 -0700

    style(layout): update HTML and body tags
    Removed suppressHydrationWarning from HTML and body tags for cleaner code.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
+    <html lang="en">
+      <body className={inter.className}>
         <div className="flex flex-col h-screen w-screen overflow-hidden">
           <Header />
           {children}
